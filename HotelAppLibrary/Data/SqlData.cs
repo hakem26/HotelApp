0cb0325773869c2b6b53747c7ff7bdf082144008
hakem26 <hamed26km@gmail.com>
2023-03-01 14:28:34 +0330

    update 1 march Max PC / new sql

--- HotelAppLibrary/Data/SqlData.cs
+++ HotelAppLibrary/Data/SqlData.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HotelAppLibrary.Databases;
+using HotelAppLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,16 @@ namespace HotelAppLibrary.Data
 {
     public class SqlData
     {
+        private readonly ISqlDataAccess _db;
+        private const string connectionStringName = "SqlDB";
+        public SqlData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+        public List<RoomTypeModel> GetAvailableRoomTypes(DateTime startDate, DateTime endDate) 
+        {
+            return _db.LoadData<RoomTypeModel, dynamic>("dbo.spRoomTypes_GetAvailableTypes", new { startDate, endDate }, connectionStringName, true);
+        }
+
     }
 }
